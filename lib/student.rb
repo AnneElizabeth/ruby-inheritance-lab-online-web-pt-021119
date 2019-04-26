0723f477670e8108431a9c6e2a0d5f4cde0d9d9f
Anne Edmondson <edmondson.anne@gmail.com>
2019-04-26 06:18:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
-require_relative "./user.rb"
-
 class Student < User
 
-end
+   attr_accessor :knowledge
+
+   def initialize
+    @knowledge = []
+  end
+
+   def learn(knowledge)
+    @knowledge << knowledge
+  end
+
+   def knowledge
+    @knowledge
+  end
+
+
+ end
